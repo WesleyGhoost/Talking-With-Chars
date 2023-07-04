16b9ce105c00ecc334618b51f995e1ae7ad401d7
Wesley <wesley.felix12@hotmail.com>
2023-07-04 17:36:23 -0300

    project config

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+"use client"
+
+import { styled } from 'styled-components';
+
+const Main = styled.main `
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding: 6rem;
+  min-height: 100vh;
+  gap: 80px
+`
+
+export default function Home() {
+  return (
+    <Main>
+      
+    </Main>
+  )
+}
